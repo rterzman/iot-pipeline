52a8f456fe5236fc43fcb7dda66923fed6da804a
Rostyslav Terzman <rostyslav.terzman@anyclip.com>
2022-11-01 15:34:41 +0200

    Feature: Dockerfile added to each service. docker-compose.yml was improved

--- api-gateway-service/src/main/java/com/iot/gateway/api/ApiGatewayIoTApplication.java
+++ api-gateway-service/src/main/java/com/iot/gateway/api/ApiGatewayIoTApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiGatewayIoTApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayIoTApplication.class, args);
     }
